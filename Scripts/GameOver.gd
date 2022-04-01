e8150a4cb69ab032f49ac82e32acd26d67dca04b
wangxin <xinxwang@thoughtworks.com>
2022-04-01 17:56:27 +0800

    update game over scene

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -1,11 +1,11 @@
 extends Popup
 
+signal start_again
+
 
 func _ready():
 	pass
 
-func _on_CloseButton_pressed():
-	hide()
-
 func _on_StartAgainButton_pressed():
 	hide()
+	emit_signal("start_again")
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -4,10 +4,14 @@ extends Control
 func _ready():
 	$Container/ScoreContainer/BestScore.set_score(Global.get_best_score())
 
-func _on_NewGame_pressed():
+func start_game():
 	Global.reset()
 	$Container/ScoreContainer/CurrentScore.set_score(Global.score)
 	$Container/Board.restart()
+
+
+func _on_NewGame_pressed():
+	start_game()
 
 func _on_Board_game_over():
 	$GameOver.show()
@@ -17,3 +21,6 @@ func _on_Board_game_over():
 
 func _on_Board_score():
 	$Container/ScoreContainer/CurrentScore.set_score(Global.score)
+
+func _on_GameOver_start_again():
+	start_game()
